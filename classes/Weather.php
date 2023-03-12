7096ae0b0d639e35269a8793da4e6340bfe38671
Nouster <djebali.mohamed88@gmail.com>
2023-03-12 15:20:19 +0100

    new class

--- classes/Weather.php
+++ classes/Weather.php
@@ -1,15 +1,39 @@
 <?php
 
-class Weather{
+class Weather {
     
+    private float $lat;
+    private float $lon;
     private string $apiKey;
+    private string $units;
+    
 
-    public function __construct(string $apiKey)
+    public function __construct(float $lat, float $long, string $apiKey, string $units)
     {
+        $this->lat = $lat;
+        $this->lon = $long;
         $this->apiKey = $apiKey;
+        $this->units = $units;
     }
 
-    public function getWeather(float $latitude, float $longitude, string $units='metric'): array{
-        
+    public function getWeather(): ?array
+    {
+        $curl = curl_init("api.openweathermap.org/data/2.5/forecast?lat=$this->lat&lon=$this->lon&appid=$this->apiKey&units=$this->units");
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $data = curl_exec($curl);
+        if(!$data){
+            return null ;
+        }
+        $results = [];
+        $data = json_decode($data, true);
+        foreach($data['list'] as $day){
+            $results = [
+                'temps' => $day['temp']['day'],
+                'description' => $day['weather'][0]['description'],
+                'date' => $day['dt']
+            ];
+            var_dump($results);
     }
+    return $results;
+}
 }
--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 require_once 'layout/header.php';
-?>
+require_once 'classes/Weather.php';
 
+$weather = new Weather(45.75, 4.85,'12d8a10ed4a8966b8d2f0e60baab934a', 'metric');
+$weather->getWeather();
 
+var_dump($weather);
 
-
-<?php
 require_once 'layout/footer.php';
 
 
